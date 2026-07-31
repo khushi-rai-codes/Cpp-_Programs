a88c1da84ddf46f84a69fc2a1e5703961019658d
Khushi <kr14052006@gmail.com>
2026-07-31 21:27:58 +0530

    Create word_frequency_map.cpp

---
+++ word_frequency_map.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <map>
+#include <sstream>
+using namespace std;
+int main()
+{
+    string sentence;
+    cout << "Enter a sentence: ";
+    getline(cin, sentence);
+    map<string, int> freq;
+    stringstream ss(sentence);
+    string word;
+    while(ss >> word)
+        freq[word]++;
+    cout << "\nWord Frequency\n";
+    for(auto x : freq)
+        cout << x.first << " : " << x.second << endl;
+    return 0;
+}
